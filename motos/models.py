8cb11c28f0e0949cec19df818e7ce862ceb21e09
rolivsan <rolivsan@gmail.com>
2019-11-17 22:03:38 -0300

    Retirado o  bd BuscaFavorito

--- motos/models.py
+++ motos/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class BuscaFavorita(models.Model):
-    usuario = models.ManyToOneRel(User, related_name="favorito")
-    montadora = models.CharField(max_length=50, null=False)
-    modelo = models.CharField(max_length=50, null=False)
-    ano =  models.CharField(max_length=4, null=False)
