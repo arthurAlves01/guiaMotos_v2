1231ec5b38f5bc35c6165413e8687c2f41fc4b84
rolivsan <rolivsan@gmail.com>
2019-11-17 22:12:02 -0300

    Adicionado BuscaFavorito

--- motos/models.py
+++ motos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Create your models here.
+class BuscaFavorita(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    montadora = models.CharField(max_length=50, null=False)
+    modelo = models.CharField(max_length=50, null=False)
+    ano =  models.CharField(max_length=4, null=False)
